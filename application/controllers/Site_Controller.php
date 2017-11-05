55ed61ed4c8c163b28a92ac49658d9850501eafe
Christian Jordan Dalan <cjdalan@gmail.com>
2017-11-05 16:53:13 +0800

    Fixed auth redirect route

--- application/controllers/Site_Controller.php
+++ application/controllers/Site_Controller.php
@@ -4,9 +4,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Site_Controller extends CI_Controller {
 
 
+	public function __construct() {
+		parent::__construct();
+	}
+
 	public function index() {
-		$this->authenticate->is_authenticated();
+		if (!$this->session->has_userdata("user")) {
+			return redirect("http://localhost/main/users/login");
+		}
+
 		$user = $this->authenticate->current_user();
+
 		$general_conversation = $this->db->get_where(
 			'chat_conversations', 
 			[
